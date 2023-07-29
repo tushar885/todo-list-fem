682a7a35cbeed0c3b10813f93794d2e3fa4002ff
tushar885 <tushar8104@gmail.com>
2023-07-29 11:30:08 +0530

    Initial Commit : set up the env

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const AppLayout = () => {
+  return <h1>Cool Hai Bhai Sab!</h1>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
